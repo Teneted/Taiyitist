048f74ee10c02d7624ebf7f8a6abe4bf90c6a0a9
wdog5 <l1531245281@yandex.com>
2025-07-20 14:04:40 +0800

    Fixed #6

--- modules/taiyitist-server/src/main/java/com/taiyitistmc/mixin/commands/MixinCommands.java
+++ modules/taiyitist-server/src/main/java/com/taiyitistmc/mixin/commands/MixinCommands.java
@@ -4,7 +4,7 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.Maps;
 import com.taiyitistmc.asm.annotation.CreateConstructor;
 import com.taiyitistmc.bukkit.BukkitDispatcher;
-import com.taiyitistmc.injection.commands.InjectionCommandNode;
+import com.taiyitistmc.fabric.CommandNodeHooks;
 import com.taiyitistmc.injection.commands.InjectionCommands;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.ParseResults;
@@ -26,6 +26,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Commands.class)
 public abstract class MixinCommands implements InjectionCommands {
@@ -124,10 +126,14 @@ public abstract class MixinCommands implements InjectionCommands {
         // Remove labels that were removed during the event
         for (String orig : bukkit) {
             if (!event.getCommands().contains(orig)) {
-                ((InjectionCommandNode) rootCommandNode).taiyitist$removeCommand(orig);
+                CommandNodeHooks.removeCommand(rootCommandNode, orig);
             }
         }
         player.connection.send(new ClientboundCommandsPacket(rootCommandNode));
     }
 
+    @Redirect(method = "fillUsableCommands", at = @At(value = "INVOKE", remap = false, target = "Lcom/mojang/brigadier/tree/CommandNode;canUse(Ljava/lang/Object;)Z"))
+    private <S> boolean taiyitist$canUse(CommandNode<S> commandNode, S source) {
+        return CommandNodeHooks.canUse(commandNode, source);
+    }
 }
