7c0beeaded6e2adebedff4cd29b460baf4e7e3ce
wdog5 <l1531245281@yandex.com>
2025-07-24 16:16:47 +0800

    Fix

--- modules/taiyitist-server/src/main/java/com/taiyitistmc/mixin/compat/stackdeobf/MixinCachedMappings.java
+++ modules/taiyitist-server/src/main/java/com/taiyitistmc/mixin/compat/stackdeobf/MixinCachedMappings.java
@@ -1,22 +1,15 @@
 package com.taiyitistmc.mixin.compat.stackdeobf;
 
-import com.taiyitistmc.config.TaiyitistConfigUtil;
 import com.taiyitistmc.util.I18n;
 import dev.booky.stackdeobf.mappings.CachedMappings;
-import dev.booky.stackdeobf.mappings.providers.AbstractMappingProvider;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.nio.file.Path;
-import java.util.concurrent.CompletableFuture;
 
 @Mixin(value = CachedMappings.class, remap = false)
 public class MixinCachedMappings {
@@ -35,13 +28,5 @@ public class MixinCachedMappings {
     @ModifyArg(method = "lambda$create$1", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;)V"), index = 0)
     private static String taiyitist$resetInfo0(String message) {
         return I18n.as("stackdeobf.cached.mappings");
-    }
-
-    @Inject(method = "create(Ljava/nio/file/Path;Ldev/booky/stackdeobf/mappings/providers/AbstractMappingProvider;)Ljava/util/concurrent/CompletableFuture;", at = @At("HEAD"), cancellable = true)
-    private static void taiyitist$configure(Path cacheDir, AbstractMappingProvider provider, CallbackInfoReturnable<CompletableFuture<CachedMappings>> cir) {
-        if (!TaiyitistConfigUtil.stackdeobf()) {
-            cir.setReturnValue(null);
-        }
-        return;
     }
 }
